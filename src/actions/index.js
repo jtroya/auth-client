089ae2d859f324c8857bbe09f0232718f8fb74a7
Jorge Troya <jorgetroya@gmail.com>
2017-09-01 18:05:43 +0200

    Updates

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,33 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import {AUTH_ERROR, AUTH_USER, UNAUTH_USER} from "./Types";
+import { AUTH_ERROR, AUTH_USER, FETCH_MESSAGE, UNAUTH_USER } from "./Types";
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
     return function (dispatch) {
         axios.post(`${ROOT_URL}/signin`, { email, password })
-            .then( response => {
+            .then(response => {
                 dispatch({ type: AUTH_USER });
                 localStorage.setItem('token', response.data.token);
-                browserHistory.push('/private');
+                browserHistory.push('/feature');
             })
             .catch(() => {
                 dispatch(authError('Bad login info'));
+            });
+    }
+}
+
+export function signupUser({ email, password }) {
+    return function (dispatch) {
+        axios.post(`${ROOT_URL}/signup`, { email, password })
+            .then(response => {
+                dispatch({ type: AUTH_USER });
+                localStorage.setItem('token', response.data.token);
+                browserHistory.push('/feature');
+            })
+            .catch(error => {
+                dispatch(authError(error.response.data.error))
             });
     }
 }
@@ -29,4 +43,18 @@ export function signoutUser() {
     localStorage.removeItem('token');
 
     return { type: UNAUTH_USER }
+}
+
+export function fetchMessage() {
+    return function (dispatch) {
+        axios.get(ROOT_URL, {
+            headers: { authorization: localStorage.getItem('token') }
+        })
+        .then(response => {
+            dispatch({
+                type: FETCH_MESSAGE,
+                payload: response.data.message
+            });
+        });
+    }
 }
--- src/index.js
+++ src/index.js
@@ -5,21 +5,33 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
+import { AUTH_USER } from "./actions/Types";
 import App from './components/App';
 import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
 import Signup from './components/auth/Signup';
+import RequireAuth from './components/auth/RequireAuth';
+import Feature from './components/Feature';
+import Welcome from './components/Welcome';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+
+if (token) {
+    store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={ createStoreWithMiddleware(reducers) }>
+  <Provider store={ store }>
       <Router history={ browserHistory }>
           <Route path="/" component={ App }>
+              <IndexRoute component={ Welcome } />
               <Route path="signin" component={ Signin }/>
               <Route path="signout" component={ Signout }/>
               <Route path="signup" component={ Signup }/>
+              <Route path="feature" component={ RequireAuth(Feature) }/>
           </Route>
       </Router>
   </Provider>
